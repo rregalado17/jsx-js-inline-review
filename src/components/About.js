e5cd9fa41bd2f2efb381fee68f777ca63e8f75ce
rregalado17 <rregalado17@gmail.com>
2021-07-24 02:39:20 -0400

    jsx review

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">About
+    <h2>About Me</h2>
+    <p>I'll get over my imposter sydrome...</p>
+    <img src={image} alt="I made this"/>
+  </div>;
 }
 
 export default About;
